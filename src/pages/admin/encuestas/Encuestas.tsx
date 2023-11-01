2f3a34afc164134600a79495540873f128e109a5
tonildo123 <tony81191@gmail.com>
2023-11-01 18:36:03 -0300

    encuentas OK

--- src/pages/admin/encuestas/Encuestas.tsx
+++ src/pages/admin/encuestas/Encuestas.tsx
@@ -6,7 +6,8 @@ import {
     Text,
     VStack,
     Center,
-    Stack
+    Stack,
+    Progress
 } from '@chakra-ui/react';
 import { Skeleton, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
 import { collection, getDocs } from 'firebase/firestore';
@@ -19,19 +20,105 @@ const Encuestas = () => {
     const [questions, setQuestions] = useState([]);
     const [loading, setLoading] = useState(true);
 
+    const questionTen = {
+        question: '10. ¿Tienes alguna sugerencia o comentario adicional sobre la usabilidad de nuestra plataforma que nos ayude a mejorar?',
+        key: 'additionalComments',
+    }
 
-    const questionMappings = {
-        '1. ¿Con qué frecuencia visitas nuestra tienda en línea de productos de plantas y jardinería?': 'frequency',
-        '2. ¿Cómo calificarías la facilidad de navegación en nuestra plataforma?': 'navigationEase',
-        '3. ¿Has encontrado que la organización de las categorías y productos es intuitiva?': 'intuitiveOrganization',
-        '4. ¿Cuál es tu opinión sobre la velocidad de carga de la página?': 'pageSpeed',
-        '5. ¿Experimentaste problemas técnicos al realizar una compra, como errores de pago o procesamiento?': 'technicalProblems',
-        '6. ¿Qué tan satisfecho estás con la información proporcionada sobre los productos, como descripciones, imágenes y precios?': 'productInformationSatisfaction',
-        '7. ¿Has utilizado la función de búsqueda en la plataforma? Si es así, ¿fue efectiva?': 'usedSearchFunction',
-        '8. ¿Te resultó fácil realizar un seguimiento de tu pedido, recibir notificaciones y acceder a información de envío?': 'trackingOrderEase',
-        '9. ¿Recomendarías nuestra tienda en línea a otras personas interesadas en productos de plantas y jardinería?': 'recommendStore',
-        '10. ¿Tienes alguna sugerencia o comentario adicional sobre la usabilidad de nuestra plataforma que nos ayude a mejorar?': 'additionalComments',
-    };
+
+    const questionMappings = [
+        {
+            question: '1. ¿Con qué frecuencia visitas nuestra tienda en línea de productos de plantas y jardinería?',
+            key: 'frequency',
+            responses: [
+                'Diariamente',
+                'Semanalmente',
+                'Mensualmente',
+                'Ocasionalmente',
+            ],
+        },
+        {
+            question: '2. ¿Cómo calificarías la facilidad de navegación en nuestra plataforma?',
+            key: 'navigationEase',
+            responses: [
+                'Muy fácil',
+                'Fácil',
+                'Neutral',
+                'Difícil',
+                'Muy difícil',
+            ],
+        },
+        {
+            question: '3. ¿Has encontrado que la organización de las categorías y productos es intuitiva?',
+            key: 'intuitiveOrganization',
+            responses: ['Sí', 'No'],
+        },
+        {
+            question: '4. ¿Cuál es tu opinión sobre la velocidad de carga de la página?',
+            key: 'pageSpeed',
+            responses: [
+                'Excelente',
+                'Buena',
+                'Aceptable',
+                'Lenta',
+                'Muy lenta',
+            ],
+        },
+        {
+            question: '5. ¿Experimentaste problemas técnicos al realizar una compra, como errores de pago o procesamiento?',
+            key: 'technicalProblems',
+            responses: ['Sí', 'No'],
+        },
+        {
+            question: '6. ¿Qué tan satisfecho estás con la información proporcionada sobre los productos, como descripciones, imágenes y precios?',
+            key: 'productInformationSatisfaction',
+            responses: [
+                'Muy satisfecho',
+                'Satisfecho',
+                'Neutral',
+                'Insatisfecho',
+                'Muy insatisfecho',
+            ],
+        },
+        {
+            question: '7. ¿Has utilizado la función de búsqueda en la plataforma? Si es así, ¿fue efectiva?',
+            key: 'usedSearchFunction',
+            responses: [
+                'Sí, fue efectiva',
+                'Sí, pero no fue efectiva',
+                'No, no la utilicé',
+            ],
+        },
+        {
+            question: '8. ¿Te resultó fácil realizar un seguimiento de tu pedido, recibir notificaciones y acceder a información de envío?',
+            key: 'trackingOrderEase',
+            responses: [
+                'Sí, fue muy fácil',
+                'Sí, pero podría mejorar',
+                'No, fue complicado',
+            ],
+        },
+        {
+            question: '9. ¿Recomendarías nuestra tienda en línea a otras personas interesadas en productos de plantas y jardinería?',
+            key: 'recommendStore',
+            responses: [
+                'Definitivamente',
+                'Probablemente',
+                'No estoy seguro',
+                'Probablemente no',
+                'Definitivamente no',
+            ],
+
+        },
+        {
+            question: '10. ¿Tienes alguna sugerencia o comentario adicional sobre la usabilidad de nuestra plataforma que nos ayude a mejorar?',
+            key: 'additionalComments',
+            responses: [''],
+        },
+
+    ];
+
+
 
     useEffect(() => {
         const fetchQuestions = async () => {
@@ -41,6 +128,7 @@ const Encuestas = () => {
                 questionDocs.push(doc.data());
             });
             setQuestions(questionDocs);
+            console.log(questionDocs)
             setLoading(false);
         };
 
@@ -72,36 +160,52 @@ const Encuestas = () => {
 
     questions.forEach((question, index) => {
         Object.entries(question).forEach(([key, value]) => {
-            const mappedKey = questionMappings[key];
-            if (!groupedAnswers[mappedKey]) {
-                groupedAnswers[mappedKey] = [];
+            const mappedQuestion = questionMappings.find(item => item.key === key);
+            if (mappedQuestion) {
+                if (!groupedAnswers[mappedQuestion.key]) {
+                    groupedAnswers[mappedQuestion.key] = [];
+                }
+                groupedAnswers[mappedQuestion.key].push({ pregunta: index + 1, respuesta: value });
             }
-            groupedAnswers[mappedKey].push({ pregunta: index + 1, respuesta: value });
         });
     });
+
+    const countResponses = (question, response) => {
+        return questions.filter(answer => answer[question.key] === response).length;
+    };
 
     return (
         <VStack minH='calc(100vh - 64px)' bgColor='gray.100' p={4}>
             <Text fontSize='xl' as='b'>RESPUESTAS A ENCUESTAS</Text>
             <Center bg='white' p={[6, 8]} borderRadius='lg' w={['sm', 'md', 'xl', '3xl']}>
                 <Accordion allowToggle>
-                    {Object.entries(questionMappings).map(([questionText, key]) => (
-                        <AccordionItem key={key}>
+                    {questionMappings.map((question) => (
+                        <AccordionItem key={question.key}>
                             <Text>
                                 <AccordionButton>
                                     <Box flex="1" textAlign="left">
-                                        {questionText}
+                                        {question.question}
                                     </Box>
                                     <AccordionIcon />
                                 </AccordionButton>
                             </Text>
                             <AccordionPanel>
-                                <List>
-                                    {questions.map((answer, index) => (
-                                        <ListItem key={index}>
-                                            <strong>Respuesta {index + 1}:</strong> {answer[key]}
-                                        </ListItem>
-                                    ))}
+                                <List style={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap' }}>
+                                    {question.key === 'additionalComments' ? (
+                                        questions.map((response, index) => (
+                                            <ListItem key={index}>
+                                                {index + 1} - {response.additionalComments}
+                                            </ListItem>
+                                        ))
+                                    ) : (
+                                        question.responses.map((response, index) => (
+                                            <ListItem key={index} style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                                <strong>{response}</strong>
+                                                <Progress value={(countResponses(question, response) / questions.length) * 100} size="sm" w="60%" colorScheme="green" />
+                                                <strong>{countResponses(question, response)}</strong>
+                                            </ListItem>
+                                        ))
+                                    )}
                                 </List>
                             </AccordionPanel>
                         </AccordionItem>
