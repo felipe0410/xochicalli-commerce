8447f9bcee249b47db88174871bcc4537cd48928
tonildo123 <tony81191@gmail.com>
2023-11-14 21:39:24 -0300

    description user OK admin OK

--- src/pages/admin/blog/DescriptionUser.tsx
+++ src/pages/admin/blog/DescriptionUser.tsx
@@ -1,16 +1,16 @@
 import {
   Box,
-  Center,
-  Image,
   Text,
   Heading,
-  VStack,
   Card,
   CardBody,
   Stack,
   IconButton,
-  CardHeader
+  UnorderedList,
+  ListItem,
+  OrderedList
 } from '@chakra-ui/react';
+import ReactPlayer from 'react-player';
 import { ArrowBackIcon } from '@chakra-ui/icons';
 import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
@@ -30,29 +30,92 @@ const DescriptionUser = () => {
   }
 
   return (
-    <VStack minH='calc(100vh - 64px)' bgColor='gray.100' p={4}>
+    <Box bgColor='gray.100' p={1}>    
+    <Box sx={{ display: 'flex', width: '100%', flexDirection: 'column', padding: 2 }}>
+      <Box sx={{ display: 'flex', width: '100%' }}>
+        <IconButton
+          fontSize='2xl'
+          aria-label='Eliminar'
+          icon={<ArrowBackIcon />}
+          onClick={() => navigate(-1)}
+        />
+      </Box>
 
-      <Center bg='white' p={[6, 8]} borderRadius='lg' w={['sm', 'md', 'xl', '3xl']}>
-        <Card key={post.id} maxW='md' borderWidth='2px' borderColor='gray.300'>
-          <CardHeader>
-            <Box sx={{ display: 'flex', width: '100%' }}>
-            <IconButton
-                aria-label='Eliminar'
-                icon={<ArrowBackIcon />}
-                onClick={() => navigate(-1)}
-              />
-            </Box>
-          </CardHeader>
-          <CardBody>
-            <Image src={post.imageURL} alt={post.title} borderRadius='lg' sx={{width:'100%'}}/>
-            <Stack mt='6' spacing='3'>
-              <Heading size='md'>{post.title}</Heading>
-              <Text>{post.description}</Text>
-            </Stack>
-          </CardBody>
-        </Card>
-      </Center>
-    </VStack>
+      <Box
+        sx={{
+          display: 'flex',
+          width: '100%',
+          height: '500px',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundImage: `url('${post.imageUrlPrincipal}')`, // Reemplaza 'url_de_la_imagen' con la URL de tu imagen
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          color: 'white', // Puedes cambiar el color del texto para que sea legible sobre la imagen
+          textShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)', // Agrega sombra al texto para mejorar la legibilidad
+        }}
+      >
+        <Text fontSize='5xl' paddingX={6} paddingY={2}
+          border='2px solid rgba(220, 220, 220, 0.5)' // Ajusta el ancho y el color del borde
+          borderRadius='8px' // 
+          backgroundColor='rgba(220, 220, 220, 0.5)' color='#196F3D' >{post.title}</Text>
+      </Box>
+
+      <Box sx={{ display: 'flex', width: '100%', flexDirection: 'column', padding: 16 }}>
+
+        <Box sx={{ display: 'flex', width: '100%', marginBottom: 8 }}>
+          <Text fontSize='2xl'>Blog / {post.category} / {post.title}</Text>
+        </Box>
+
+        <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+          <Card key={post.id} borderWidth='2px' borderColor='gray.300'>
+            <CardBody>
+              <Stack mt='6' spacing='3'>
+                <Heading size='md'>Contenido</Heading>
+                {post.contenidos.map((contenido: any, index: any) => (
+                  <Box key={index} sx={{ paddingX: 8 }}>
+                    <OrderedList>
+                      <ListItem>{contenido.subtitulo}</ListItem>
+                    </OrderedList>
+                  </Box>
+                ))}
+              </Stack>
+            </CardBody>
+          </Card>
+        </Box>
+
+        <Box sx={{ display: 'flex', width: '100%', marginY: 8 }}>
+          <Text fontSize='2xl'>{post.description}</Text>
+        </Box>
+
+        <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+          <Stack mt='6' spacing='3'>
+            {post.contenidos.map((contenido: any, index: any) => (
+              <Box key={index} sx={{ paddingX: 8 }}>
+                <Text fontSize='4xl'>{contenido.subtitulo}</Text>
+                <UnorderedList spacing={2}>
+                  {contenido.detalle.map((detalle: any, detalleIndex: any) => (
+                    <ListItem key={detalleIndex} fontSize='xl'>{detalle}</ListItem>
+                  ))}
+                </UnorderedList>
+              </Box>
+            ))}
+          </Stack>
+        </Box>
+
+
+        <Box sx={{ display: 'flex', width: '100%', marginY: 8 }}>
+          <ReactPlayer
+            url={post.youtubeLink}
+            width="100%"
+            controls={true}
+          />
+        </Box>        
+
+      </Box>
+    </Box>
+  </Box>
   );
 };
 
