8541d0594b57c6b6eeb2c20522359e47c6e546cd
felipe0410 <andres.montana.r4@gmail.com>
2023-11-29 22:42:33 -0500

    storage cache-3

--- vite.config.ts
+++ vite.config.ts
@@ -6,57 +6,53 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), eslint(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: [
-        'favicon.ico',
-        'robots.txt',
-        'apple-touch-icon.png',
-        'icons/*.png', // Adjust this pattern based on your icon filenames and paths
-        'images/**/*', // Include images directory
+  VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: [
+      'favicon.ico',
+      'robots.txt',
+      'apple-touch-icon.png',
+      'icons/*.png', // Ajusta este patrón según los nombres y rutas de tus iconos
+      'images/**/*', // Incluye el directorio de imágenes
+    ],
+    manifest: {
+      name: 'Tu Nombre de la Aplicación',
+      short_name: 'App',
+      description: 'Descripción de tu aplicación',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
       ],
-      manifest: {
-        name: 'Your App Name',
-        short_name: 'App',
-        description: 'Your app description',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/icons/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+    },
+    workbox: {
+      globPatterns: [
+        '**/*.{js,ico,png,svg,html,css}',
+        'images/**/*', // Incluye el directorio de imágenes
+      ],
+      runtimeCaching: [
+        {
+          urlPattern: /\.(?:png|jpg|jpeg|gif|svg)$/,
+          handler: 'CacheFirst',
+        },
+        {
+          urlPattern: /^https:\/\/firebasestorage\.googleapis\.com\/.*$/,
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'firebase-storage',
           },
-          {
-            src: '/icons/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-      workbox: {
-        globPatterns: [
-          '**/*.{js,ico,png,svg,html,css}',
-          'images/**/*', // Include images directory
-        ],
-        runtimeCaching: [
-          {
-            urlPattern: /\.(?:png|jpg|jpeg|gif|svg)$/,
-            handler: 'CacheFirst',
-          },
-          {
-            urlPattern: /^https:\/\/firebasestorage\.googleapis\.com\//,
-            handler: 'StaleWhileRevalidate',
-            options: {
-              cacheName: 'firebase-storage',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-              },
-            },
-          },
-        ],
-      },
-    })
+        },
+      ],
+    },
+  }),
   ],
   server: {
     watch: {
